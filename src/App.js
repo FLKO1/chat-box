0d10f2cb020bee3d587f2d6f4ae2cf98fa76da52
FLKO1 <83712998+FLKO1@users.noreply.github.com>
2024-08-26 02:07:49 -0400

    CREACION DE PROYECTO

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import React from 'react';
 import './App.css';
 
+type Mensaje = {
+  text: String;
+  sender: 'ai'| 'user'
+}
+
 function App() {
+  const[ mensaje,setMensaje]= useState<Mensaje[]>([
+    {
+      text: "Mensaje de ejemplo",
+      sender: "ai"
+    },
+    {
+      text: "Mensaje de ejemplo",
+      sender: "user"
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Soporte IA</h1>
+      <p className='message ai'>¿Problemas con algun dispositivo?</p>
+      <p className='message user'>¿No puede ingresar a una carpeta?</p>
+      <p className='message ai'>Puede realizar aca su consulta y se le solucionara</p>
+      <p className='message user'>En caso de no tener solucion, contacte con el equipo de Inovacion</p>
+    <form className='input-form'>
+      <input type="text"  placeholder='Ingrese su problema'></input>
+      <input type="submit"  placeholder='Enviar'></input>
+    </form>
     </div>
+    
   );
 }
 
